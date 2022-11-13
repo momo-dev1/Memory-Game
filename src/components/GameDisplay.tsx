686f7663124d7e95c52abefb0be0a80ec4c30c08
momo-dev1 <didy1234567@gmail.com>
2022-11-13 19:23:23 +0200

    finalizing app

--- src/components/GameDisplay.tsx
+++ src/components/GameDisplay.tsx
@@ -55,29 +55,40 @@ const GameDisplay = (): JSX.Element => {
   }, [score]);
 
   useEffect(() => {
+    if (selectedCards.length === 2) {
+      if (randomCards) {
+        setTurns(turns + 1);
+
+        let newState: CardObj[] = [];
+        if (selectedCards[0].name === selectedCards[1].name) {
+          setTimeout(() => cardFoundSFX.play(), 1000);
+          setScore(score + 1);
+          newState = randomCards.map((card) => {
+            if (card.name === selectedCards[0].name) {
+              return { ...card, found: true, flipped: false };
+            }
+            return card;
+          });
+        } else {
+          newState = randomCards.map((card) => {
+            if (
+              card.name === selectedCards[0].name ||
+              card.name === selectedCards[1].name
+            ) {
+              return { ...card, flipped: false };
+            }
+            return card;
+          });
+        }
+        setTimeout(() => {
+          setRandomCards(newState);
+          setSelectedCards([]);
+        }, 1000);
+      }
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedCards]);
 
-  const setFlippedStatus = (cardIndex: number) => {
-    if (randomCards) {
-      if (
-        randomCards[cardIndex].flipped === true ||
-        randomCards[cardIndex].found === true
-      ) {
-        return;
-      }
-
-      const newState = randomCards.map((card, index) => {
-        if (index === cardIndex) {
-          return { ...card, flipped: true };
-        }
-        return card;
-      });
-      setRandomCards(newState);
-    }
-  };
-  const updateSelectedCards = (card: CardObj) => { };
-  
   const handleClick = (cardIndex: number, card: CardObj) => {
     if (isPlaying) {
       if (selectedCards.length < 2) {
@@ -89,7 +100,27 @@ const GameDisplay = (): JSX.Element => {
     }
   };
 
-  
+  const setFlippedStatus = (cardIndex: number) => {
+    if (randomCards) {
+      let isFlipped = randomCards[cardIndex].flipped === true;
+      let isFound = randomCards[cardIndex].found === true;
+      if (isFlipped || isFound) return;
+
+      const newState = randomCards.map((card, index) => {
+        if (index === cardIndex) {
+          return { ...card, flipped: true };
+        }
+        return card;
+      });
+      setRandomCards(newState);
+    }
+  };
+
+  const updateSelectedCards = (card: CardObj) => {
+    if (card.flipped === true) return;
+    if (card.found === true) return;
+    setSelectedCards([...selectedCards, card]);
+  };
 
   const resetGame = () => {
     setScore(0);
